cc5e516677665f3ec4a393ed80a601830f5132c4
Lays-Dev <penn2ink@gmail.com>
2026-01-30 15:40:47 -0500

    Player Movement Script
    Wrote and implemented player movement.
    Added Level1 scene.
    Added player temp sprite.
    Added an Input Setting
    Removed old Input Setting and scenes
    Changed camera FOV

---
+++ Unity Project here/Prototype1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private float verticalInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    public void OnMoveVertical(InputValue value)
+    {
+        verticalInput = value.Get<float>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(0f, verticalInput * moveSpeed);
+    }
+}
