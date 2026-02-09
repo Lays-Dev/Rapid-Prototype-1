6aa7fc9b298f8581af9683af00f0ff20c37e1141
Dominic Thorp <dthorp1379@gmail.com>
2026-02-09 16:13:15 -0500

    Updated Scenes with Crawl and Skip Button

--- Unity Project here/Prototype1/Assets/Scripts/CrawlText.cs
+++ Unity Project here/Prototype1/Assets/Scripts/CrawlText.cs
@@ -1,11 +1,23 @@
 using UnityEngine;
-
-public class TextCrawl : MonoBehaviour
+using UnityEngine.SceneManagement;
+//This adds a section in the inspector to change scroll speed and what scene comes after
+public class CrawlText : MonoBehaviour
 {
-    public float scrollSpeed = 30f;
+    public float scrollSpeed = 15f;
+    public float endYPosition = 1000f;
+    public string nextScene;
 
+    //This loads next scene at a position I specify
     void Update()
-    {
-        transform.Translate(Vector3.up * scrollSpeed * Time.deltaTime);
+    {transform.Translate(Vector3.up * scrollSpeed * Time.deltaTime);
+
+        if (transform.position.y >= endYPosition)
+        {
+            LoadNextScene();
+        }
     }
+
+//This is for skip button
+    public void LoadNextScene()
+    {SceneManager.LoadScene(nextScene);}
 }
